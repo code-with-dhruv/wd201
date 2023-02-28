ea0512b9f4124123090518daccc97c02b338e275
dhruv <87694090+code-with-dhruv@users.noreply.github.com>
2023-03-01 02:28:17 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,33 +1,48 @@
-const h_server = require("h_server");
-const f_server = require("f_server");
-const args = require("minimist")(process.argv);
-let homes_contents = "";
-let pro_contents = "";
-let reg_contents = "";
+const h_server = require("http");
+const f_server = require("fs");
+let minimists = require('minimist')
 
+let hms_cont = "";
+let project_cont = "";
+let regis_cont = "";
+
+let you =5000;
+let me= 3000;
+
+
+
+let args = minimists(process.argv.slice(2));
 f_server.readFile("home.html", (errors, home) => {
   if (errors) {
     throw errors;
   }
   else{
-    homes_contents = home;
+    hms_cont = home;
   }
   
 });
 
-f_server.readFile("project.html", (errors, project) => {
-  if (errors) {
-    throw errors;
-  }
-  pro_contents = project;
-});
+
 
 f_server.readFile("registration.html", (errors, registration) => {
   if (errors) {
     throw errors;
   }
-  reg_contents = registration;
+  regis_cont = registration;
 });
+
+
+let varrr="started"
+
+
+f_server.readFile("project.html", (errors, project) => {
+  if (errors) {
+    throw errors;
+  }
+  project_cont = project;
+});
+
+
 
 h_server
   .createServer((request, response) => {
@@ -35,17 +50,30 @@ h_server
     response.writeHeader(200, { "Content-Type": "text/html" });
     switch (url) {
       case "/project":
-        response.write(pro_contents);
+        response.write(project_cont);
         response.end();
         break;
-      case "/registration.html":
-        response.write(reg_contents);
+      case "/registration":
+        response.write(regis_cont);
         response.end();
         break;
+      case "/home":
+        response.write(hms_cont);
+        response.end();
+        break;
+
+      case "/homee":
+        response.write(hms_cont);
+        response.end();
+        break;
+      case "/Home":
+          response.write(hms_cont);
+          response.end();
+          break;
       default:
-        response.write(homes_contents);
+        response.write(hms_cont);
         response.end();
         break;
     }
-  })
-  .listen(args.port);
+  }).listen(args.port, () => {
+    console.log(" at   http://localhost:" + `${args.port}`)});
